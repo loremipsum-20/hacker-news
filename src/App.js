2f76d728d30bc7403b71ea6f9dd358ccc827ebcc
Janine <janine.duhre@googlemail.com>
2021-04-24 10:46:04 +0200

    initial commit, page set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
+import News from './components/News/News.js'
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Hello world!
+        </h1>
+        <News />
     </div>
   );
 }
---
+++ src/components/News/News.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function News () {
+  return (
+  <div>
+    <ol>
+    <li>I'm a news headline</li>
+    </ol>
+  </div>
+)}
